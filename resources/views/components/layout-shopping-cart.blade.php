68fb60155506f194e7a19402e295bb19b12f30c4
yulius k wijaya <70045649+yulius98@users.noreply.github.com>
2025-05-04 10:47:03 +0700

    commit33

--- resources/views/components/layout-shopping-cart.blade.php
+++ resources/views/components/layout-shopping-cart.blade.php
@@ -3,7 +3,22 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    @php
+	        $isProduction = app()->environment('production');
+	        $manifestPath = $isProduction ? '../public_html/build/manifest.json' : public_path('build/manifest.json');
+    @endphp
+
+    @if ($isProduction && file_exists($manifestPath))
+	        @php
+		        $manifest = json_decode(file_get_contents($manifestPath), true);
+	        @endphp
+	        <link rel="stylesheet" href="{{config('app.url')}}/build/{{$manifest['resources/css/app.css']['file']}}">
+	        <script type="module" src="{{config('app.url')}}/build/{{$manifest['resources/js/app.js']['file']}}"></script>
+    @else
+	        @viteReactRefresh
+	        @vite(['resources/js/app.js','resources/css/app.css'])
+    @endif
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://unpkg.com/feather-icons"></script>
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,7 +3,23 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
+    @php
+	        $isProduction = app()->environment('production');
+	        $manifestPath = $isProduction ? '../public_html/build/manifest.json' : public_path('build/manifest.json');
+    @endphp
+
+    @if ($isProduction && file_exists($manifestPath))
+	        @php
+		        $manifest = json_decode(file_get_contents($manifestPath), true);
+	        @endphp
+	        <link rel="stylesheet" href="{{config('app.url')}}/build/{{$manifest['resources/css/app.css']['file']}}">
+	        <script type="module" src="{{config('app.url')}}/build/{{$manifest['resources/js/app.js']['file']}}"></script>
+    @else
+	        @viteReactRefresh
+	        @vite(['resources/js/app.js','resources/css/app.css'])
+    @endif
+    
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script src="https://unpkg.com/feather-icons"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
